91b430c2815d1f7b164cb6ee26bec10e265c03db
Sam Malanchuk <malanchuksam@gmail.com>
2020-06-03 21:36:55 -0400

    setup Redux environment

--- text-em/src/index.js
+++ text-em/src/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom'
+import reducer from './redux/reducer'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 require('dotenv').config();
 
+const store = createStore(
+  reducer,
+  compose(
+  applyMiddleware(
+          thunk,
+          ),
+),
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
---
+++ text-em/src/redux/reducer.js
@@ -0,0 +1,15 @@
+// import action types
+// import {
+
+// } from './actions'
+
+// state initial values
+const initialState = {
+}
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+        default:
+            return state
+    }
+}
