645bf81656eee99073af08a35643badd556d0012
Sam Malanchuk <malanchuksam@gmail.com>
2020-06-09 20:01:25 -0400

    imported action types into the reducer and added the switch cases for sending sms

--- text-em/src/redux/actions.js
+++ text-em/src/redux/actions.js
@@ -2,9 +2,27 @@
 import axios from 'axios';
 
 // define action types
-export const SEND_TEXT_START = 'SEND_TEXT_START';
-export const SEND_TEXT_SUCCESS = 'SEND_TEXT_SUCCESS';
-export const SEND_TEXT_FAILED = 'SEND_TEXT_FAILED';
+export const SEND_SMS_START = 'SEND_SMS_START';
+export const SEND_SMS_SUCCESS = 'SEND_SMS_SUCCESS';
+export const SEND_SMS_FAILED = 'SEND_SMS_FAILED';
 
 // define the base API URL
 const API_URL = 'http://localhost:4070';
+
+// action creators
+
+// send sms function makes an API request to send an SMS to a number
+export function send_sms(payload) {
+    return (dispatch) => {
+        dispatch({ type: SEND_SMS_START })
+
+        return axios.post(`${API_URL}/send`, payload)
+            .then((res) => {
+                dispatch({ type: SEND_SMS_SUCCESS, payload: res.data })
+            })
+            .catch((err) => {
+                console.log('error seems to be', err)
+                dispatch({ type: SEND_SMS_FAILED, payload: err })
+            })
+    }
+}
